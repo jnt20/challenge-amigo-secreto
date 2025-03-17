460d707e6043a7c3e9fb79bb9f49b147273049ea
Jnt20 <jhonatanperezcunya@gmail.com>
2025-03-16 21:38:59 -0500

    primer

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log("Ejecutando el sistema")
+
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigo = [];
+
+function agregarAmigo() {
+    let imputAmigo = document.getElementById("amigo");
+    let nombreAmigo = imputAmigo.value;
+
+    if (!nombreAmigo) {
+        alert("Por favor, ingrese un nombre");
+        return;
+    }
+
+    amigo.push(nombreAmigo);
+    imputAmigo.value = "";
+    imputAmigo.focus();
+    renderizarAmigos();
+}
+
+function renderizarAmigos () {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for( let i = 0; i < amigo.length; i++){
+         let item = document.createElement("li");
+         item.textContent = amigo[i];
+         listaAmigos.appendChild(item)
+    }
+
+}
+
+function sortearAmigo() {
+    if (amigo.length === 0){
+        alert("No hay amigos para sortear");
+        return;
+     }
+    let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+    let resultado = document.getElementById("resultado")
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+
+    let limpiarLista = document.getElementById("listaAmigos")
+    limpiarLista.innerHTML ="";
+}
+
